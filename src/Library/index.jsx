9b1c8890eb2851468becc572bbc167091c6acaf5
sigvegodoy <sigvegrevling@gmail.com>
2022-08-27 11:07:52 +0200

    add exports

---
+++ src/Library/index.jsx
@@ -0,0 +1,17 @@
+import Menu from './Menu/Menu';
+import { SettingsButton } from './Buttons/SettingsButton'
+import { MenuButton } from './Buttons/MenuButton'
+import { RadioButton, RadioGroup } from './Radio'
+import { CheckBox } from './CheckBox'
+import { ToggleSwitch } from './ToggleSwitch'
+
+const exports = {
+    Menu,
+    SettingsButton,
+    MenuButton,
+    RadioButton,
+    RadioGroup,
+    CheckBox,
+    ToggleSwitch
+}
+export default exports;
