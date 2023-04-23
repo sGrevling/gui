97c01ceba50a5c8fc061533f0006c78fbb0df6eb
sigvegodoy <sigvegrevling@gmail.com>
2023-04-23 12:37:56 +0200

    fix exports

--- src/Library/index.jsx
+++ src/Library/index.jsx
@@ -1,14 +1,31 @@
 import UseEffectOnce from "./utils/UseEffectOnce";
 import UseEffectPartialDeps from "./utils/UseEffectPartialDeps";
-export { UseEffectOnce, UseEffectPartialDeps};
-export { Menu } from './Menu/Menu';
-export { SettingsButton } from './Buttons/SettingsButton'
-export { MenuButton } from './Buttons/MenuButton'
-export { ResetButton } from './Buttons/ResetButton'
-export { RadioButton, RadioGroup } from './Radio'
-export { CheckBox } from './CheckBox'
-export { Dots } from './Dots/Dots'
-export { SunMoon } from './SunMoon'
-export { ToggleSwitch } from './ToggleSwitch'
-export { NumberSelect } from './NumberSelect/NumberSelect'
-export { PaneView } from './PaneView/PaneView'
+
+import { Menu } from './Menu/Menu';
+import { SettingsButton } from './Buttons/SettingsButton';
+import { MenuButton } from './Buttons/MenuButton';
+import { ResetButton } from './Buttons/ResetButton';
+import { RadioButton, RadioGroup } from './Radio';
+import { CheckBox } from './CheckBox';
+import { Dots } from './Dots/Dots';
+import { SunMoon } from './SunMoon';
+import { ToggleSwitch } from './ToggleSwitch';
+import { NumberSelect } from './NumberSelect/NumberSelect';
+import { PaneView } from './PaneView/PaneView';
+
+export {
+    UseEffectOnce,
+    UseEffectPartialDeps,
+    Menu,
+    SettingsButton,
+    MenuButton,
+    ResetButton,
+    RadioButton,
+    RadioGroup,
+    CheckBox,
+    Dots,
+    SunMoon,
+    ToggleSwitch,
+    NumberSelect,
+    PaneView
+};
